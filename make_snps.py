86bca9062132ffd5489b16aedf0ca48e7eaf8909
jingyaq <jingya.qiu.16@dartmouth.edu>
2015-11-11 16:26:28 -0500

    11-11-15 JQ
    updated make_snps.py

--- make_snps.py
+++ make_snps.py
@@ -1,6 +1,12 @@
+import itertools
+
 # sample: pass in sample class object (contains snps information right??)
 # combo_size: pass in size of SNP combos (1-4)
 def make_snpCombos (sample, combo_size):
+
+  # TEST CODE
+# def make_snpCombos (combo_size):
+#   snpList = ['SNPA','SNPB','SNPC','SNPD','SNPE','SNPF','SNPG']
 
   # read in a sample, append each individual SNP in dataset to snpList[]
   snpList = []
@@ -8,90 +14,33 @@ def make_snpCombos (sample, combo_size):
   for snp in sample.snps:
     snpList.append(snp)
 
-
   # generate list of SNP combos for interaction testing (1, 2, 3, or 4)
   comboList = []
 
   # generate SNP combo possibilities of 1
   if (combo_size == 1):
-    comboList = snpList
+    for subset in itertools.combinations(snpList,1):
+      comboList.append(subset)
+
 
   # generate SNP combo possibilities of 2
   if (combo_size == 2):
 
-    for i in range (len(snpList)):
-      if i <= len(snpList)-2:
-        snps = []
-        snps.append(snpList[i])
-        snps.append(snpList[i+1])
-        comboList.append(snps)
+    for subset in itertools.combinations(snpList,2):
+      comboList.append(subset)
 
-      # wrap around
-      else:
-        snps = []
-        snps.append(snpList[i])
-        snps.append(snpList[0])
-        comboList.append(snps)
 
   # generate SNP combo possibilities of 3
   if (combo_size == 3):
 
-    for i in range (len(snpList)):
-      if i <= len(snpList)-3:
-        snps = []
-        snps.append(snpList[i])
-        snps.append(snpList[i+1])
-        snps.append(snpList[i+2])
-        comboList.append(snps)
+    for subset in itertools.combinations(snpList,3):
+      comboList.append(subset)
 
-      elif i == len(snpList)-2:
-        snps = []
-        snps.append(snpList[i])
-        snps.append(snpList[i+1])
-        snps.append(snpList[0])
-        comboList.append(snps)
-
-      elif i == len(snpList)-1:
-        snps = []
-        snps.append(snpList[i])
-        snps.append(snpList[0])
-        snps.append(snpList[1])
-        comboList.append(snps)
 
   # generate SNP combo possibilities of 4
   if (combo_size == 4):
 
-    for i in range (len(snpList)):
-      if i <= len(snpList)-4:
-        snps = []
-        snps.append(snpList[i])
-        snps.append(snpList[i+1])
-        snps.append(snpList[i+2])
-        snps.append(snpList[i+3])
-        comboList.append(snps)
-
-      elif i == len(snpList)-3:
-        snps = []
-        snps.append(snpList[i])
-        snps.append(snpList[i+1])
-        snps.append(snpList[i+2])
-        snps.append(snpList[0])
-        comboList.append(snps)
-
-      elif i == len(snpList)-2:
-        snps = []
-        snps.append(snpList[i])
-        snps.append(snpList[i+1])
-        snps.append(snpList[0])
-        snps.append(snpList[1])
-        comboList.append(snps)
-
-      elif i == len(snpList)-1:
-        snps = []
-        snps.append(snpList[i])
-        snps.append(snpList[0])
-        snps.append(snpList[1])
-        snps.append(snpList[2])
-        comboList.append(snps)
+    for subset in itertools.combinations(snpList,4):
+      comboList.append(subset)
 
   return comboList
