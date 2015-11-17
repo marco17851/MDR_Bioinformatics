0136f988430ef21187889961768081b066b64e02
Danielle Midulla <danielle.midulla@gmail.com>
2015-11-17 12:47:29 -0500

    removing the loop checking for all dimensions up to and including n in the x-validation

--- xval.py
+++ xval.py
@@ -1,3 +1,6 @@
+"""
+@author: Danielle
+"""
 from selected_sample import SelectedSample
 from random import shuffle
 from make_snps import make_snpCombos
@@ -8,15 +11,15 @@ from predictor import getPrediction
 MAX_NUM_SNPS = 4
 NUM_GENOTYPES = 3
 
-def xval(sample_list, phenotype_numbers, nfold, ndimensions = 1, threshold = 1.0):
+def xval(sample_list, phenotype_numbers, num_folds, ndimensions = 1, threshold = 1.0):
     error_rates = {}
     shuffled_samples = list(sample_list)
     shuffle(shuffled_samples)
-    folds = [[] for i in range(nfold)]
+    folds = [[] for i in range(num_folds)]
 
     #fill in the folds
     for i in range(len(shuffled_samples)):
-        folds[i%nfold].append(shuffled_samples[i])
+        folds[i % num_folds].append(shuffled_samples[i])
 
     #for each fold, perform mdr using the fold as the test case
     for i in range(len(folds)):
@@ -35,7 +38,7 @@ def xval(sample_list, phenotype_numbers, nfold, ndimensions = 1, threshold = 1.0
             else:
                 error_rates[snp_combo] = current_error_rates[snp_combo]
     #divide the error rates by the number of folds in order to average them
-    error_rates = {x: error_rates[x]/nfold for x in error_rates}
+    error_rates = {x: error_rates[x] / num_folds for x in error_rates}
     return error_rates
 
 
@@ -44,30 +47,29 @@ def mdr(test, train, phenotype_numbers, ndimensions = 1, threshold = 1.0):
     Args:
       train (list of Sample): possible neighbors whose labels will be used
       test (list of Sample): instances whose labels will be inferred from neighbors
+      ndimensions (integer): the number of snps we want to look at in each cell
+      threshold (float): the threshold at whcih we differentiate between high- and low-riskq
     Returns:
-      dictionary: the error rate for every possible snp combination
+      dictionary: the error rate for every possible combination of ndimensions snps
     """
     error_rates = {}
-    highest_num_snps = min(MAX_NUM_SNPS, ndimensions)
-    #for every possible number of dimensions (1-4)
-    for num_snps in range(1, highest_num_snps+1):
-        combo_list = make_snpCombos(train[0], num_snps)
-        #for every possible combination of n-dimensional snps
-        for combo in combo_list:
-            #make the cell
-            current_cell = Cell()
-            keys = make_keys(NUM_GENOTYPES, num_snps)
-            # Make n-dimensional 'space' matrix where each cell represents a unique combination of genotypes at SNPs_OF_INTEREST
-            dict_c = current_cell.make_cells(keys, num_snps)
-            # Calculate case, control, ratio, risk of all cells in matrix
-            dict_d = current_cell.calc_cells(train, phenotype_numbers, combo, dict_c, threshold)
-            #make the predictions for the current test set
-            pred_list = getPrediction(dict_d, test, combo)
-            #calculate the error rate and save to dictionary
-            incorrect_count = 0
-            for i in range(len(test)):
-                if test[i].phenotype != pred_list[i]:
-                    incorrect_count += 1.
-            error_rates[combo] = float(incorrect_count)/float(len(test))
+    num_snps = min(MAX_NUM_SNPS, ndimensions)
+    combo_list = make_snpCombos(train[0], num_snps)
+    #for every possible combination of n-dimensional snps
+    for combo in combo_list:
+        #make the keys
+        keys = make_keys(NUM_GENOTYPES, num_snps)
+        # Make n-dimensional 'space' matrix where each cell represents a unique combination of genotypes at SNPs_OF_INTEREST
+        dict_c = Cell.make_cells(keys, num_snps)
+        # Calculate case, control, ratio, risk of all cells in matrix
+        dict_d = Cell.calc_cells(train, phenotype_numbers, combo, dict_c, threshold)
+        #make the predictions for the current test set
+        pred_list = getPrediction(dict_d, test, combo)
+        #calculate the error rate and save to dictionary
+        incorrect_count = 0
+        for i in range(len(test)):
+            if test[i].phenotype != pred_list[i]:
+                incorrect_count += 1.
+        error_rates[combo] = float(incorrect_count)/float(len(test))
     #return the error rate dictionary
     return error_rates
